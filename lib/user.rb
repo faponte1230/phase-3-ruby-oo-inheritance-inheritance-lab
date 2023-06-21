77875b26d26e59dcda5dc7ef0d3874c32abcfa4b
Fernando Aponte <faponte1230@yahoo.com>
2023-06-20 20:56:11 -0400

    commit tree 12098027358a16891840178a665f8eec63c0ef7d

---
+++ lib/user.rb
@@ -0,0 +1,23 @@
+class User 
+
+    def initialize
+        @first_name = first_name
+        @last_name = last_name
+    end
+
+    def first_name
+        @first_name
+    end
+
+    def first_name=(name)
+        @first_name = name
+    end
+
+    def last_name
+        @last_name
+    end
+
+    def last_name=(name)
+        @last_name = name
+    end
+end
